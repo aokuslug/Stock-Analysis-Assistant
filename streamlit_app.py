f340ecbffbeb3d9d1ac6dff55d72e602485a8c17
aokuslug <aliokuslug@gmail.com>
2026-09-25 08:32:53 +0200

    Create initial Stock Analysis Assistant interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Stock Analysis Assistant",
+    layout="wide"
 )
+
+st.title("Stock Analysis Assisstant")
+
+st.write (
+    "Research US-listed stocks using technical, fundamental, "
+    "and market data."
+)
+
+ticker = st.text_input(
+    "Enter a stock ticker",
+    placeholder="Example:AAPL"
+)
+
+if st.button("Analyze"):
+    if ticker:
+        st.success(f"Selected stock: {ticker.upper()}")
+    else:
+        st.warning("Please enter a stock ticker.")
